6e75ff2c644ecbb89a6cd0b3280a142d434e7a64
cxiaobai <cxb233@outlook.com>
2024-11-17 21:47:06 +0800

    (写编码器模块:添加Ex_encoder相关文件|添加基本结构和函数)

---
+++ components/devices/Ex_encoder/Ex_ecd_protocol.h
@@ -0,0 +1,4 @@
+#ifndef __EX_ECD_PROTOCOL_H__
+#define __EX_ECD_PROTOCOL_H__
+
+#endif
---
+++ components/devices/Ex_encoder/Ex_encoder.h
@@ -0,0 +1,53 @@
+#ifndef __EX_ECD__
+#define __EX_ECD__
+
+typedef struct __External_ecd_handler_t External_ecd_handler_t;
+typedef struct __External_ecd_bus_handler_t External_ecd_bus_handler_t;
+
+typedef enum{
+  NONE_ECD=0,
+  ECD_USART_COMMUNICATION,
+  ECD_CAN_COMMUNICATION
+} ecd_protocol_type_e;
+
+typedef enum{
+  /*can通信*/
+  OID_ECD=1,
+
+  ___DIVIDING__LINE___,
+  /*usart通信*/
+} ecd_type_e;
+
+struct __External_ecd_handler_t{
+  ecd_protocol_type_e protocol_type;
+  ecd_type_e type;
+
+  /*CAN总线专用*/
+  FDCAN_HandleTypeDef* can;
+  uint16_t id;
+
+  /*usart专用*/
+  unsigned int (*USART_Recv)(uint8_t *,unsigned short);
+};
+
+struct __External_ecd_bus_handler_t{
+  External_ecd_handler_t* can_communication_ecd[6];
+  External_ecd_handler_t* usart_communication_ecd[3];
+
+  uint16_t can_ecd_count;
+  uint16_t usart_ecd_count;
+};
+
+/*只能有一个*/
+extern External_ecd_bus_handler_t Ex_ecd_bus;
+
+void External_ecd_bus_init(void);
+void External_ecd_bus_append(External_ecd_handler_t* ecd,ecd_type_e type);
+
+uint32_t External_ecd_get_value(External_ecd_handler_t* ecd);
+fp32 External_ecd_get_angle(External_ecd_handler_t* ecd);
+
+void __External_ecd_usart_feedback_hook(void);
+void __External_ecd_can_feedback_hook(uint16_t id);
+
+#endif
