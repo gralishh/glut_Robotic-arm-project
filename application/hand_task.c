472253879017fd97bd98e9a37a57ee1b378d9365
cxiaobai <cxb233@outlook.com>
2024-12-01 23:04:37 +0800

    (构建机构任务框架:移植任务模板)

--- application/hand_task.c
+++ application/hand_task.c
@@ -2,4 +2,17 @@
 #include "hand_task_interface.h"
 #include "motor_timer_ctrl.h"
 
+void hand_task(void *argument)
+{
+  hand_task_init();
+  osDelay(1000)
+  while(1)
+  {
+    hand_task_get_feedback();
+    hand_task_mode_flush();
+    hand_task_set_output();
+    hand_task_output();
+    osDelay(1);
+  }
+}
 
--- application/hand_task.h
+++ application/hand_task.h
@@ -1,7 +1,52 @@
 #ifndef _HAND_TASK_H
 #define _HAND_TASK_H
-#include "main.h"
-#include "hand_task_interface.h"
 
+#include "main.h"
+#include <stdint.h>
+#include "general_motor_module.h"
+#include "task.h"
+
+typedef enum{
+  first_joint=0x00,
+  JOINT_COUNT,
+}HAND_JOINT_INDEX;
+
+typedef enum{
+  first_motor,
+  MOTOR_COUNT,
+}HAND_MOTOR_INDEX;
+
+typedef enum{
+  first_mode=0x00,
+  MODE_COUNT,
+}HAND_CTRL_MODE;
+
+typedef struct{
+  /*instance array*/
+  void* motor_instance[MOTOR_COUNT];
+
+  /*state value*/
+  uint8_t ctrl_mode;
+  Motor_Type_e motor_type[MOTOR_COUNT];
+  Motor_Ctrl_mode_e motor_ctrl_mode[MOTOR_COUNT];
+
+  /*feedback value*/
+  fp32 feedback_motor_angle[MOTOR_COUNT];
+  int32_t feedback_motor_speed[MOTOR_COUNT];
+  int32_t feedback_motor_current[MOTOR_COUNT];
+
+  /*output value*/
+  fp32 motor_angle[MOTOR_COUNT];
+  int32_t motor_speed[MOTOR_COUNT];
+  int32_t motor_current[MOTOR_COUNT];
+
+  /*for arm ctrl*/
+  fp32 feedback_joint_angle[JOINT_COUNT];
+  fp32 joint_angle[JOINT_COUNT];
+} HAND_TASK_HANDLER_TYPE; 
+HAND_TASK_HANDLER_TYPE hand_task_handler;/*unique structure*/
+HAND_TASK_HANDLER_TYPE* hand_task_handler_ptr=&hand_task_handler;
+
+void hand_task(void *argument);
 
 #endif
