91c8d08b7707c5c9ec0507a6d8521b57762f5e24
cxiaobai <cxb233@outlook.com>
2024-12-29 20:12:38 +0800

    *(modify:修改了底盘电机id)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -253,16 +253,16 @@ int main(void)
   defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
 
   /* creation of ChassisTask */
-  //ChassisTaskHandle = osThreadNew(__chassis_task, NULL, &ChassisTask_attributes);
+  ChassisTaskHandle = osThreadNew(__chassis_task, NULL, &ChassisTask_attributes);
 
   /* creation of USART2_measure */
   USART2_measureHandle = osThreadNew(__usart2_measure_task, NULL, &USART2_measure_attributes);
 
   /* creation of GimbalTask */
-  GimbalTaskHandle = osThreadNew(__gimbal_task, NULL, &GimbalTask_attributes);
+  //GimbalTaskHandle = osThreadNew(__gimbal_task, NULL, &GimbalTask_attributes);
 
   /* creation of HandTask */
-  HandTaskHandle = osThreadNew(__hand_task, NULL, &HandTask_attributes);
+  //HandTaskHandle = osThreadNew(__hand_task, NULL, &HandTask_attributes);
 
   /* creation of USART3_measure */
   //USART3_measureHandle = osThreadNew(__usart3_measure_task, NULL, &USART3_measure_attributes);
--- application/chassis_task_interface.c
+++ application/chassis_task_interface.c
@@ -89,26 +89,26 @@ void chassis_task_init()
   /*电机初始化*/
   __SET_MOTOR_INSTANCE(DJI_LF,&DJI_Motor_LeftFront);
   __SET_MOTOR_TYPE(DJI_LF,DJI_MOTOR);
-  DJI_Motor_init(&DJI_Motor_LeftFront,&DJI_CAN3_Bus_ctrl,M3508,0x205);
+  DJI_Motor_init(&DJI_Motor_LeftFront,&DJI_CAN3_Bus_ctrl,M3508,0x201);
   DJI_Motor_Speed_PID_init(&DJI_Motor_LeftFront,PID_POSITION,25,0,0,5000,0);
 
   __SET_MOTOR_INSTANCE(DJI_RF,&DJI_Motor_RightFront);
   __SET_MOTOR_TYPE(DJI_RF,DJI_MOTOR);
-  DJI_Motor_init(&DJI_Motor_RightFront,&DJI_CAN3_Bus_ctrl,M3508,0x205);
+  DJI_Motor_init(&DJI_Motor_RightFront,&DJI_CAN3_Bus_ctrl,M3508,0x202);
   DJI_Motor_Speed_PID_init(&DJI_Motor_RightFront,PID_POSITION,25,0,0,5000,0);
 
   __SET_MOTOR_INSTANCE(DJI_LB,&DJI_Motor_LeftBack);
   __SET_MOTOR_TYPE(DJI_LB,DJI_MOTOR);
-  DJI_Motor_init(&DJI_Motor_LeftBack,&DJI_CAN3_Bus_ctrl,M3508,0x205);
+  DJI_Motor_init(&DJI_Motor_LeftBack,&DJI_CAN3_Bus_ctrl,M3508,0x204);
   DJI_Motor_Speed_PID_init(&DJI_Motor_LeftBack,PID_POSITION,25,0,0,5000,0);
 
   __SET_MOTOR_INSTANCE(DJI_RB,&DJI_Motor_RightBack);
   __SET_MOTOR_TYPE(DJI_RB,DJI_MOTOR);
-  DJI_Motor_init(&DJI_Motor_RightBack,&DJI_CAN3_Bus_ctrl,M3508,0x205);
+  DJI_Motor_init(&DJI_Motor_RightBack,&DJI_CAN3_Bus_ctrl,M3508,0x203);
   DJI_Motor_Speed_PID_init(&DJI_Motor_RightBack,PID_POSITION,25,0,0,5000,0);
 
   __chassis_idle_ctrl();
-  DJI_CANBus_enable_bus(&DJI_CAN3_Bus_ctrl);
+  //DJI_CANBus_enable_bus(&DJI_CAN3_Bus_ctrl);
 }
 
 /**
