32271087560c0cc059cca25457bc9fe8c214cf82
cxiaobai <cxb233@outlook.com>
2025-01-10 23:44:29 +0800

    (add|modify:添加气路gpio输出|添加底盘位置环pid|修改自锁逻辑)

--- application/chassis_task_interface.c
+++ application/chassis_task_interface.c
@@ -90,22 +90,26 @@ void chassis_task_init()
   __SET_MOTOR_INSTANCE(DJI_LF,&DJI_Motor_LeftFront);
   __SET_MOTOR_TYPE(DJI_LF,DJI_MOTOR);
   DJI_Motor_init(&DJI_Motor_LeftFront,&DJI_CAN1_Bus_ctrl,M3508,0x201);
-  DJI_Motor_Speed_PID_init(&DJI_Motor_LeftFront,PID_POSITION,25,0,0,5000,0);
+  DJI_Motor_Speed_PID_init(&DJI_Motor_LeftFront,PID_POSITION,25,0,0.001,5000,0);
+  DJI_Motor_Pos_PID_init(&DJI_Motor_LeftFront,PID_POSITION,20,0,0,500,0);
 
   __SET_MOTOR_INSTANCE(DJI_RF,&DJI_Motor_RightFront);
   __SET_MOTOR_TYPE(DJI_RF,DJI_MOTOR);
   DJI_Motor_init(&DJI_Motor_RightFront,&DJI_CAN1_Bus_ctrl,M3508,0x202);
-  DJI_Motor_Speed_PID_init(&DJI_Motor_RightFront,PID_POSITION,25,0,0,5000,0);
+  DJI_Motor_Speed_PID_init(&DJI_Motor_RightFront,PID_POSITION,25,0,0.001,5000,0);
+  DJI_Motor_Pos_PID_init(&DJI_Motor_RightFront,PID_POSITION,20,0,0,500,0);
 
   __SET_MOTOR_INSTANCE(DJI_LB,&DJI_Motor_LeftBack);
   __SET_MOTOR_TYPE(DJI_LB,DJI_MOTOR);
   DJI_Motor_init(&DJI_Motor_LeftBack,&DJI_CAN1_Bus_ctrl,M3508,0x204);
-  DJI_Motor_Speed_PID_init(&DJI_Motor_LeftBack,PID_POSITION,25,0,0,5000,0);
+  DJI_Motor_Speed_PID_init(&DJI_Motor_LeftBack,PID_POSITION,25,0,0.001,5000,0);
+  DJI_Motor_Pos_PID_init(&DJI_Motor_LeftBack,PID_POSITION,20,0,0,500,0);
 
   __SET_MOTOR_INSTANCE(DJI_RB,&DJI_Motor_RightBack);
   __SET_MOTOR_TYPE(DJI_RB,DJI_MOTOR);
   DJI_Motor_init(&DJI_Motor_RightBack,&DJI_CAN1_Bus_ctrl,M3508,0x203);
-  DJI_Motor_Speed_PID_init(&DJI_Motor_RightBack,PID_POSITION,25,0,0,5000,0);
+  DJI_Motor_Speed_PID_init(&DJI_Motor_RightBack,PID_POSITION,25,0,0.001,5000,0);
+  DJI_Motor_Pos_PID_init(&DJI_Motor_RightBack,PID_POSITION,20,0,0,500,0);
 
   __chassis_idle_ctrl();
   DJI_CANBus_enable_bus(&DJI_CAN1_Bus_ctrl);
@@ -227,19 +231,16 @@ void __chassis_idle_ctrl()
   int index;
   for(index=0;index<CHASSIS_MOTOR_COUNT;index++)
   {
-    __SET_MOTOR_LOCKUP(index);
+    __SET_MOTOR_ANGLE(index,0.0);
   }
 }
 
 void __chassis_rc_ctrl()
 {
+  /*设置输入速度(not real)*/
   HANDLER_PTR->vx=-RC_CTRL_PTR->rc.ch[3]*VX_CTRL_SEN;
   HANDLER_PTR->vy=-RC_CTRL_PTR->rc.ch[2]*VY_CTRL_SEN;
   HANDLER_PTR->wz=-RC_CTRL_PTR->rc.ch[0]*WZ_CTRL_SEN;
-
-  //HANDLER_PTR->vx=HANDLER_PTR->vx
-  //HANDLER_PTR->vy=HANDLER_PTR->vy
-  //HANDLER_PTR->wz=HANDLER_PTR->wz
 
   __SET_MOTOR_SPEED(DJI_LF, - HANDLER_PTR->vx - HANDLER_PTR->vy + ( CHASSIS_WZ_SET_SCALE - 1.0f) * MOTOR_DISTANCE_TO_CENTER * HANDLER_PTR->wz);
   __SET_MOTOR_SPEED(DJI_RF,   HANDLER_PTR->vx - HANDLER_PTR->vy + ( CHASSIS_WZ_SET_SCALE - 1.0f) * MOTOR_DISTANCE_TO_CENTER * HANDLER_PTR->wz);
