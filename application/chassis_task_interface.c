e3c85ce4f2f66319792c20051ff281cb9c2625a4
cxiaobai <cxb233@outlook.com>
2025-01-11 21:12:04 +0800

    (add:添加底盘抬升联合控制|)

--- application/chassis_task_interface.c
+++ application/chassis_task_interface.c
@@ -5,6 +5,7 @@
 #include "remote_control.h" 
 #include "angle_process.h"
 #include "cmsis_os2.h"
+#include "detect_task.h"
 
 #define HANDLER chassis_task_handler
 #define HANDLER_PTR chassis_task_handler_ptr
@@ -33,6 +34,7 @@ DJI_Motor_Ctrl_t DJI_Motor_RightBack;
 static void __chassis_nonforce(void);
 static void __chassis_idle_ctrl(void);
 static void __chassis_rc_ctrl(void);
+static void __chassis_union_rc_ctrl(void);
 
 
 /*general handler method*/
@@ -90,25 +92,25 @@ void chassis_task_init()
   __SET_MOTOR_INSTANCE(DJI_LF,&DJI_Motor_LeftFront);
   __SET_MOTOR_TYPE(DJI_LF,DJI_MOTOR);
   DJI_Motor_init(&DJI_Motor_LeftFront,&DJI_CAN1_Bus_ctrl,M3508,0x201);
-  DJI_Motor_Speed_PID_init(&DJI_Motor_LeftFront,PID_POSITION,25,0,0.001,5000,0);
+  DJI_Motor_Speed_PID_init(&DJI_Motor_LeftFront,PID_POSITION,20,0,0.001,5000,0);
   DJI_Motor_Pos_PID_init(&DJI_Motor_LeftFront,PID_POSITION,20,0,0,500,0);
 
   __SET_MOTOR_INSTANCE(DJI_RF,&DJI_Motor_RightFront);
   __SET_MOTOR_TYPE(DJI_RF,DJI_MOTOR);
   DJI_Motor_init(&DJI_Motor_RightFront,&DJI_CAN1_Bus_ctrl,M3508,0x202);
-  DJI_Motor_Speed_PID_init(&DJI_Motor_RightFront,PID_POSITION,25,0,0.001,5000,0);
+  DJI_Motor_Speed_PID_init(&DJI_Motor_RightFront,PID_POSITION,20,0,0.001,5000,0);
   DJI_Motor_Pos_PID_init(&DJI_Motor_RightFront,PID_POSITION,20,0,0,500,0);
 
   __SET_MOTOR_INSTANCE(DJI_LB,&DJI_Motor_LeftBack);
   __SET_MOTOR_TYPE(DJI_LB,DJI_MOTOR);
   DJI_Motor_init(&DJI_Motor_LeftBack,&DJI_CAN1_Bus_ctrl,M3508,0x204);
-  DJI_Motor_Speed_PID_init(&DJI_Motor_LeftBack,PID_POSITION,25,0,0.001,5000,0);
+  DJI_Motor_Speed_PID_init(&DJI_Motor_LeftBack,PID_POSITION,20,0,0.001,5000,0);
   DJI_Motor_Pos_PID_init(&DJI_Motor_LeftBack,PID_POSITION,20,0,0,500,0);
 
   __SET_MOTOR_INSTANCE(DJI_RB,&DJI_Motor_RightBack);
   __SET_MOTOR_TYPE(DJI_RB,DJI_MOTOR);
   DJI_Motor_init(&DJI_Motor_RightBack,&DJI_CAN1_Bus_ctrl,M3508,0x203);
-  DJI_Motor_Speed_PID_init(&DJI_Motor_RightBack,PID_POSITION,25,0,0.001,5000,0);
+  DJI_Motor_Speed_PID_init(&DJI_Motor_RightBack,PID_POSITION,20,0,0.001,5000,0);
   DJI_Motor_Pos_PID_init(&DJI_Motor_RightBack,PID_POSITION,20,0,0,500,0);
 
   __chassis_idle_ctrl();
@@ -158,6 +160,11 @@ void chassis_task_mode_flush()
     else if(switch_is_up(get_remote_control_point()->rc.s[0]))
       __SET_STRUCT_MODE(CHASSIS_MODE_IDLE);
   }
+  else if(switch_is_mid(get_remote_control_point()->rc.s[1]))
+  {
+    if(switch_is_mid(get_remote_control_point()->rc.s[0]))
+      __SET_STRUCT_MODE(CHASSIS_MODE_UNION_CTRL);
+  }
   else
   {
     __SET_STRUCT_MODE(CHASSIS_MODE_IDLE);
@@ -188,6 +195,9 @@ void chassis_task_set_output()
       break;
     case CHASSIS_MODE_RC_CTRL:
       __chassis_rc_ctrl();
+      break;
+    case CHASSIS_MODE_UNION_CTRL:
+      __chassis_union_rc_ctrl();
       break;
     case CHASSIS_MODE_NONFORCE:
     default:
@@ -241,6 +251,19 @@ void __chassis_rc_ctrl()
   HANDLER_PTR->vx=-RC_CTRL_PTR->rc.ch[3]*VX_CTRL_SEN;
   HANDLER_PTR->vy=-RC_CTRL_PTR->rc.ch[2]*VY_CTRL_SEN;
   HANDLER_PTR->wz=-RC_CTRL_PTR->rc.ch[0]*WZ_CTRL_SEN;
+
+  __SET_MOTOR_SPEED(DJI_LF, - HANDLER_PTR->vx - HANDLER_PTR->vy + ( CHASSIS_WZ_SET_SCALE - 1.0f) * MOTOR_DISTANCE_TO_CENTER * HANDLER_PTR->wz);
+  __SET_MOTOR_SPEED(DJI_RF,   HANDLER_PTR->vx - HANDLER_PTR->vy + ( CHASSIS_WZ_SET_SCALE - 1.0f) * MOTOR_DISTANCE_TO_CENTER * HANDLER_PTR->wz);
+  __SET_MOTOR_SPEED(DJI_RB,   HANDLER_PTR->vx + HANDLER_PTR->vy + (-CHASSIS_WZ_SET_SCALE - 1.0f) * MOTOR_DISTANCE_TO_CENTER * HANDLER_PTR->wz);
+  __SET_MOTOR_SPEED(DJI_LB, - HANDLER_PTR->vx + HANDLER_PTR->vy + (-CHASSIS_WZ_SET_SCALE - 1.0f) * MOTOR_DISTANCE_TO_CENTER * HANDLER_PTR->wz);
+}
+
+void __chassis_union_rc_ctrl()
+{
+  /*设置输入速度(not real)*/
+  HANDLER_PTR->vx=-RC_CTRL_PTR->rc.ch[3]*VX_CTRL_SEN;
+  HANDLER_PTR->vy=-RC_CTRL_PTR->rc.ch[2]*VY_CTRL_SEN;
+  HANDLER_PTR->wz=0;
 
   __SET_MOTOR_SPEED(DJI_LF, - HANDLER_PTR->vx - HANDLER_PTR->vy + ( CHASSIS_WZ_SET_SCALE - 1.0f) * MOTOR_DISTANCE_TO_CENTER * HANDLER_PTR->wz);
   __SET_MOTOR_SPEED(DJI_RF,   HANDLER_PTR->vx - HANDLER_PTR->vy + ( CHASSIS_WZ_SET_SCALE - 1.0f) * MOTOR_DISTANCE_TO_CENTER * HANDLER_PTR->wz);
